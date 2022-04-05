463345efffc8ad5c4804894b888194fa519fd7a4
Alexander <xander1620@gmail.com>
2022-04-04 20:29:23 -0400

    SIS457LAB102

---
+++ principal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Mi nombre es Marlon Alexander Morodias Bohorquez."<<endl;
+	cout << "Estudio Ing. Sistemas en la usfx"<<endl;
+	cout << "Naci en Potosi el 10 de agosto de 1998. " << endl;
+	cout << "y no pude asistir a la clase donde se explico el lab 01 de la materia. Ing. disculpe si la tarea no era asi" << endl;
+	system("pause");
+	return 0;
+}
